448d222939f16981b668adbafec62dfcc7cea7d4
sofia agaeva <sofiaagaeva4574@gmail.com>
2022-10-16 23:07:02 +0700

    create project

---
+++ src/components/main-wrapper.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'wrapper'
+}
+</script>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+</style>
