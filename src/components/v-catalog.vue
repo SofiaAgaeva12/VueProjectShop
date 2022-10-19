c21f85b553129e6f5a8d978d32e02d5faa80dc0f
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-10-19 13:53:26 +0700

    creation of pagination

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,11 +1,10 @@
 <template>
   <div class='v-catalog'>
     <h1>Catalog of goods</h1>
-
     <div
-        v-for="product in this.products"
-        :key="product.id"
-    >
+      v-for="product in this.products"
+      :key="product.id"
+  >
       <div class="v-catalog-item">
         <img src="" alt="">
         <p class="v-catalog-item__name">{{ product.name }}</p>
@@ -14,7 +13,8 @@
         <p>Price: {{ product.price }}</p>
         <button class="product-button">Add to cart</button>
       </div>
-      </div>
+
+ </div>
 
   </div>
 </template>
@@ -26,10 +26,13 @@ import axios from "axios";
 export default {
   name: "v-catalog",
   data() {
-    return {products:undefined}
+    return {
+      products:undefined,
+      currentPage: 1,
+    };
+
   },
   components: {
-
   },
   computed: {
     // ...mapGetters([
@@ -40,8 +43,37 @@ export default {
     // ...mapActions([
     //   'GET_PRODUCTS_FROM_API',
     // ]),
+  breakProducts() {
+    let products = this.products
+    let size = 10;
+    let subarray = [];
+    let subarray2 = [];
+    let subarray3 = [];
+    let subarray4 = [];
+    let subarray5 = [];
+    let subarray6 = [];
+    let subarray7 = [];
+    let subarray8 = [];
+    let subarray9 = [];
+    let subarray10 = [];
+    while (products.size) {
+      subarray.push(products.splice(0, size))
+      for (let elements = size; elements <= size*2; elements++) {subarray2.push(products.splice(0, size))}
+      for (let elements = size*2; elements <= size*3; elements++) {subarray3.push(products.splice(0, size))}
+      for (let elements = size*3; elements <= size*4; elements++) {subarray4.push(products.splice(0, size))}
+      for (let elements = size*4; elements <= size*5; elements++) {subarray5.push(products.splice(0, size))}
+      for (let elements = size*5; elements <= size*6; elements++) {subarray6.push(products.splice(0, size))}
+      for (let elements = size*6; elements <= size*7; elements++) {subarray7.push(products.splice(0, size))}
+      for (let elements = size*7; elements <= size*8; elements++) {subarray8.push(products.splice(0, size))}
+      for (let elements = size*8; elements <= size*9; elements++) {subarray9.push(products.splice(0, size))}
+      for (let elements = size*9; elements <= size*10; elements++) {subarray10.push(products.splice(0, size))}
+        }
+
+
+
+  }
   },
-  mounted () {
+  mounted() {
     axios
         .get('https://jurapro.bhuser.ru/api-shop/products')
         .then(response => {
@@ -50,6 +82,7 @@ export default {
         )
   }
 }
+
 </script>
 
 <style>
@@ -78,6 +111,37 @@ export default {
   border:  none;
   border-radius: 3px;
   color: white;
+}
+
+
+a { cursor: pointer; }
+.pagination {
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.pagination-container {
+  display: flex;
+  column-gap: 10px;
+}
+.paginate-buttons {
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: rgb(242, 242, 242);
+  border: 1px solid rgb(217, 217, 217);
+  color: black;
+}
+.paginate-buttons:hover {
+  background-color: #d8d8d8;
+}
+.active-page {
+  background-color: #3498db;
+  border: 1px solid #3498db;
+  color: white;
+}
+.active-page:hover {
+  background-color: #2988c8;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import store from './vuex/store'
 import Vue from 'vue'
 // import Paginate from "vuejs-paginate-next"
-
+// import JwPagination from 'jw-vue-pagination';
+import "vue-awesome-paginate/dist/style.css";
 const app = createApp(App)
 
 // app.component('Paginate', Paginate)
 
 app.use(store)
-
+// app.component('jw-pagination', JwPagination);
 app.mount('#app')
 
 
